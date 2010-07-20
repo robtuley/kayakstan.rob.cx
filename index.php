4aae78edfb47f18e81d3337fe9dbacedac4067cd
Rob Tuley <rob@openknot.com>
2010-07-20 18:00:46 +0100

    Created basic single info page template & content

---
+++ index.php
@@ -0,0 +1,223 @@
+<?php header('Content-Type:text/html; charset=utf8'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Kayak in Kyrgyzstan</title>
+    <meta charset=utf-8>
+    <link rel=stylesheet media=screen,projection
+          href=css/screen.css>
+    <script src=http://www.google.com/jsapi></script>
+    <script>
+    if (window.google && window.google.load) {
+        google.load('jquery','1.4');
+    } else {
+        var e = document.createElement('script');
+        e.src = 'js/jquery.js';
+        document.getElementsByTagName('head')[0].appendChild(e);
+    }
+    </script>
+    <!--[if IE]><script src=js/html5.js></script><![endif]-->
+</head>
+<body>
+<h1>Kayaking in Kyrgyzstan</h1>
+
+<p>These notes are a result of the British University Kayak Expedition (BUKE) to Kyrgyzstan in the summer of 2005. We hope you find them useful.</p>
+
+<h2>When to go</h2>
+
+<p>July and August are the hottest months, and due to the glacier coverage they are also the time of year when rivers are at their highest. Depending on what sort of levels you want, the paddling season runs from late June (very high) to mid-September (dropping off). We visited the country in the later portion of this season (mid-August to mid-September), and although doubtless we were able to paddle some rivers which would have been too high if we'd gone earlier, we concluded that going a month earlier would have yielded better paddling.</p>
+
+<h2>How to get there</h2>
+
+<p>It is possible to fly into either Bishkek (Kyrgyz capital) or Almaty (actually in Kazakstan, but very close to the border), and BA fly direct to both from London Heathrow. A cheaper option is to fly via Moscow, with Aeroflot being the major carrier on this route.</p>
+
+<h2>Travelling around</h2>
+
+<p>We didn't experiment with local public transport, but I think it would be a real struggle. Couple that with the fact that most rivers are in very sparsely populated areas and I think it would be virtually impossible. Most of the Russians who paddle this region drive a car down themselves. If you're flying in, it's probably best to hire a vehicle and a driver. For a small group, a 4x4 (usually a Lada Niva) would be ideal, we went of bit OTT and hired 2 monster trucks from <a href=http://www.karakol.kg>Turkestan</a>.</p>
+
+<h2>Language</h2>
+
+<p>The local language is Kyrgyz which is very close to Russian. We hired a Russian-English interpreter, and for a large group such as ours he was worth his weight in gold. It is worth remembering that if you hire a driver, they will often be Russian, and communication may be a problem. For buying food/etc. our interpreter was useful, but not essential. Don't, whatever you do, think that anyone local will speak English. They won't.</p>
+
+<h2>Weather</h2>
+
+<p>Is reasonably mild. The paddling season is the Kyrgyz summer, so during the day the sun's generally shining (although expect some rain), and temperature pleasant. You will need to pack warm gear, and a good snug sleeping bag - many river locations are at a high altitude, and you will probably spend most time between 1500-3000m altitude... during the night it will often drop below freezing.</p>
+
+<h2>Accommodation</h2>
+
+<p>We camped out the back of the trucks for most of the trip, and I recommend you at least take camping gear. In the larger centres there are plenty of hostels, which make for a nice change however.</p>
+
+<h2>Money</h2>
+
+<p>Although there are ATM machines in Bishkek (which take both Visa and Maestro), you'll rarelly find them outside major cities. Travellers cheques are a real no-go too - you will only be able to change them at the banks in Bishkek, and they charge an 'admin' commission. Dollars cash is probably your best bet - there are currency exchange booths all over the place. Large denomination notes will get the best exchange rates, and make sure your notes are printed recently and in good condition - they wouldn't accept notes older than 1994 when we were there, and certainly not torn or marked notes.</p>
+
+<h2>Visas</h2>
+
+<p>If you are a UK national, you need a visa to visit Kyrgyzstan. There is a <a href=http://www.kyrgyz-embassy.org.uk>Kyrgyz Embassy</a> in the UK in London at which a visa can be obtained in advance. It is not imperative that you get the visa before you go - the same visa is available when you arrive at Manas Bishkek airport. If you are flying into Almaty, you will also need a <a href=http://kazakhstanembassy.org.uk>Kazahstan visa</a>.</p>
+
+<h2>Phone/Internet</h2>
+
+<p>Standard mobile phones will work in some areas, connecting into the local <a href=http://www.bitel.kg>Bitel</a> network. Internet cafes are widely available in the larger towns, and shared dialup (very slow) connections can often be found in smaller towns too.</p>
+
+<h2>Maps</h2>
+
+<p>There is an excellent map 'shop' in Bishkek itself, which you should be able to find by asking around once you get there. This sells 1:200,000 topographical maps to the entire country, with 80m contour lines, and are probably the easiest maps to work with on a day-to-day basis.</p>
+
+<p><a href=http://www.stanfords.co.uk>Stanfords</a> sell a couple of large scale maps, the best for the north-Eastern area is the tourist map of Eastern Kyrgyzstan.</p>
+
+<p>In addition to the normal online Google mapping, a full set of 1:100,000 topographical maps to the country are hosted by <a href=http://www.lib.berkeley.edu/EART/topo.html>Berkely Earth Sciences and Map Library</a>.</p>
+
+<h2>Resources</h2>
+
+<p>Some other sources of information are:</p>
+<ul>
+<li>The main <a href=http://www.kayaking.ru/en/rivers/tienshan>Russian kayaking site has good guides to the area (in English)</a>. These were published just after we got back from our trip.</li>
+<li>The site <a href=http://www.whitewater.ru>www.whitewater.ru</a> has loads of info, but all in Russian. For example, if you'd like to <a href=http://www.whitewater.ru/tourism/Rivers/MiddleAsya/Saryjaz/Sary99.htm>scare yourself on the Sary-Jaz</a>...</li>
+<li>A previous <a href=http://kyrgyzkayak.co.uk>UK trip to the country</a> ended with a shoulder dislocation after two weeks.</li>
+<li>The <a href=http://www.tetongravity.com>TGR team</a> were out there about the same time (July 2003), and some footage of Kyrgyzstan appears in their '<a href=http://www.arris.biz>One World</a>' DVD release. When we got in touch, they said the Naryn Gorge is an excellent big volume multiday trip, and its tributaries (the Big and Little Naryn) also harbour some excellent whitewater. We agree.</li>
+<li>Vladimir Gavrilov has written a good rafting orientated guidebook (in English) to the rivers of central Asia, entitled 'Rivers of an Unknown Land'. This includes some larger volume runs in Kyrgzstan (e.g. Naryn). The book is available from <a href=http://www.canotier.com>Canotier</a>.</li>
+</ul>
+
+<h2>Rivers</h2>
+<script>
+$(function() {
+  $('section.river').children().hide()
+     .filter('h4')
+       .show()
+       .click(function(){
+          $(this).siblings().show();
+        });
+});
+</script>
+
+<p>The size of rivers vary greatly in Kyrgyzstan, from steep low volume creeks to medium-big volume whitewater. These notes result are from atrip between the 20th August to the 20th September. As a result most of the rivers were running at medium/low levels. Earlier in the summer (June/July) we believe the levels would be much higher.</p>
+
+<h3>South of Bishkek</h3>
+
+<p>The rivers flowing towards Bishkek are low volume and seem to go at different times of year, The Ala-Archa would probably be great earlier in the season with more water, whereas the Ala-Medin was ideal mid-August. Driving up the Kara-Balta to the pass we would guess that much more water would be required to make this run. The East and West Karakol rivers which flow south from the same mountains appeared to be too low at this time of year and would require more water, the Kerkermeren however was at an ideal level, and would go with both more (scary) and less water. The Chong-Kemin lies to the east of Biskek, running almost parallel with the border with Kazakhstan.</p>
+
+<section class=river>
+<h4>Ala Archa (above border post), Gd3/4</h4>
+<p>Glacier Fed. Too low in late August, need to catch it earlier in the season. The river runs through a National Park, which has a small entrance fee. A good quality dirt track runs alongside the river, and is only ~2hrs from Bishkek.</p>
+<p>A ditch run that looks exciting due to its never ending speed but lacks river features to make the run interesting and unique. In higher levels though, the speed could make this a testing run.</p>
+</section>
+
+<section class=river>
+<h4>Alamedin, 14km Gd3/4</h4>
+<p>The river is glacier fed, although there seems to be little level change
+throughout the day. We encountered ideal levels in late august, by mid-September it was too low. Good quality road runs close to the river, and being close to Bishkek makes it an ideal warm-up run.</p>
+<p>Put-in at the end of the road that runs up beside the river. It is also possible to walk up with the boats on the path on river right -- we walked about a km. The first 5km is Gd4, then the river gets slightly easier. You can paddle all the way back down to the town, or get out at any of the bridges that cross the river to shorten the run.</p>
+</section>
+
+<section class=river>
+<h4>Chong Kemin, 20km Gd3-5</h4>
+<p>Access via tarmac road degrading to good dirt track above Kayndy village. There is a vehicle barrier low down the valley - foreigners are 'supposed' to pay 300 som per person, we negotiated down to 50. Higher up the valley the local yurt dweller had his own barrier and wanted to see the receipt.</p>
+<h5>Middle, 10km Gd3/4</h5>
+<p> A suggested put in is the highest bridge in the valley, just below an obvious braided section 76deg,41. Get out can be at the sickle shaped bend in river (76deg,22). After the river has rested on the upper plain, it gathers pace to Gd2/3, soon you come to a bridge at 76deg,35. It isn't soon after this that the river reaches Gd4, clean lines and fair paced water. Another section of Gd 3 follows with the best section soon after. About 4km of Gd4/4+ around 76deg,26 technical rapids around bends, pourovers and shoots all make this a delight. The river settles down until the suggested egress at 76deg,22.</p>
+<h5>Lower, 10km Gd4(5)</h5>
+<p>Get in at the sickle shaped bend in river (76deg,22) and out where road meets river at 76deg,15. Above and below the crux gorge can be seen from the road, and the entry rapid is probably the hardest on this section.</p>
+</section>
+
+<section class=river>
+<h4>Kerkermeren, 30km Gd3-4</h4>
+<p>The river has a large catchment area and seems to be rain fed. Either way, it still had plenty of water in at the beginning of September, and I suspect is good throughout the entire season.
+<h5>Upper, 15km Gd4</h5>
+<p>Driving southwards from the turn-off from the Bishkek to Osh main road, put in 2km above the road bridge as the river starts to look interesting. Take out at the small village downstream. The river starts with some nice grade 3, some bigger grade 4+ rapids requiring inspection after 4km or so, all happily with alternative chicken lines. The river flattens out as it approaches the village.</p>
+<h5>Middle, 15km Gd3/4</h5>
+<p>Put in at the village used as a takeout for the upper section. Take
+out at the first bridge after the major confluence approx 15km downstream (ish). Flat for the first few km after the village, two gorge sections, the first
+~500m long, the second slightly longer, with a large almost-river-wide hole hidden within it.</p>
+<p>It may be possible to continue all the way down to the confluence with the Naryn, although we didn't paddle that far and would probably be a multi-day. From the maps it looks to have some more interesting looking gorge-like sections</p>
+<h5></h5>
+</section>
+
+<h3>South of lake Issyk-Kul</h3>
+
+<p>In general the best rivers in this region are towards the eastern end of the lake, though in higher water some of the western rivers may also provide some good boating. The levels we found towards the end of August were ideal for some rivers, but others could have done with perhaps a little more, meaning coming earlier in the season. However, more water in a number of the rivers we explored would have made things a bit too fast to stop before the log-jam portages typical of this region.</p>
+
+<section class=river>
+<h4>Arashan, 10km Gd4/4+</h4>
+<p>Drive up the road from Ak-Suu village until you eventually reach the Arashan 'Health-Spa'. A night here is recommended, allowing you to relax for an evening in the hot springs. This also leaves ample time to complete this long section, don't underestimate the drive time up the road. Take-out at the road bridge, 1km upstream of a barrage.</p>
+</section>
+
+<section class=river>
+<h4>Barskoön, 5.5km Gd4/5</h4>
+<p>A good quality dirt road leads up the river from Barskoön. Apparantly the reason for the good quality of this track is that there is a goldmine further up the valley at Kumtor. The road weaves back and forth over the river, and the sections are outlined in relation to the bridge number (counted up from one as we drove up from the main Issyk-Kul lake road).</p>
+<dl>
+<dt>Bridge 6 to 5, 2.5km Gd4/4+</dt>
+<dd>A few hundred metres of grade 3 leads to a forested section where the river drops away steeply into a mini gorgelike section. Inspection is possible by cimbing out of your boat in microeddies and scrambling along the riverbank. This section is very continuous and in higher water would probably be non stop grade 5, with trees.</dd>
+<dt>Bridge 5 to 4, 1.5km Gd2</dt>
+<dd>Straightforward shallow rapids, not much gradient, essentially linking the harder upper and lower sections.</dd>
+<dt>Bridge 4 to 3, 2km Gd3/4</dt>
+<dd>A short read-and-run blast through another forested section. Some rapids to keep you on your toes, but nothing henious requiring portages.Trees seem to be less of a problem here than the steep section above.</dd>
+<dt>Bridge 3 to 2, 1.5km Gd5/6</dt>
+<dd>After a nice drop immediately after the bridge, the gnarl starts 200m into  the section and continues for another 200m or so. At the medium/low waterlevels we inspected it at there were numerous obvious siphons and drops landing on rock. Higher water may make this possible, but it would be a hell of a full on ride. It is recommended to have a look at this from the path down from the road on river left before even starting the section. The portage is not easy and may require ropes to abseil back into the gorge below. From here it appears to be grade 3/4, but we took out in the end so this isn't 100% sure.</dd>
+</dl>
+</section>
+
+<section class=river>
+<h4>Chong-Kyzyl-Suu</h4>
+</section>
+
+<section class=river>
+<h4>Jeti-Öghüz</h4>
+</section>
+
+<section class=river>
+<h4>Juuka</h4>
+</section>
+
+<section class=river>
+<h4>Karakol</h4>
+</section>
+
+<section class=river>
+<h4>Turgen-Ak-Suu</h4>
+</section>
+
+<h3>Catchment of Sary-Jaz</h3>
+
+<p>The entire Sary-Jaz area can currently be accessed by only one road. This road crosses the chong-Ashuu pass which is 3822m above sea level. The road over the top is a dirt track, but of reasonable quality, however when we crossed this at the end of August and it still had snow covering the top 100m or so of the pass. On the other side the road between the pass and the Enilchek confluence is tarmaced and in a good condition, except in a few places where landslides have fallen into the road. The rest of the roads in area are dirt tracks and considerably less well maintained. Some bridges shown on our maps were unsuitable for use, severely limiting exploration of the area by vehicle. The area appears to be very sparsely populated, with no shops or petrol stations to the south of the pass. Plan to be entirely self-sufficient and take lots of spare fuel! The rivers in this region were low at the end of August when we paddled them, but earlier in the season there may be too much snow to get over the pass to paddle them. In higher water some of these rivers would be excellent fun.</p>
+
+<section class=river>
+<h4>Sary-Jaz</h4>
+</section>
+
+<section class=river>
+<h4>Kaiyngdy</h4>
+</section>
+
+<section class=river>
+<h4>Kerolu</h4>
+</section>
+
+<section class=river>
+<h4>Terekty</h4>
+</section>
+
+<h3>Naryn and its tributaries</h3>
+
+<p>The Naryn is one big long river, running for at least a few hundred kilometres. We explored the area above and around Naryn town itself. I particularly enjoyed this area, with the Kichi-Naryn and Song-Kol being the highlights of my trip.</p>
+
+<section class=river>
+<h4>Naryn</h4>
+</section>
+
+<section class=river>
+<h4>Kichi-Naryn</h4>
+</section>
+
+<section class=river>
+<h4>Song-Köl</h4>
+</section>
+
+<h3>Further South</h3>
+
+<p>We didn't explore any further south than Naryn, but rumours abound of sweet granite slides around Osh. The southern area of Kyrgyzstan is less stable than the north, mostly as you're much closer to the borders with the other 'stans so it will worth checking up on the current FCO advice before planning to travel there. If anyone has any info on this area, I would be interested to hear it so please <a href=http://openknot.com/me>get in touch</a>.</p>
+
+<footer>
+    <p class=copy><small>© 2010 <a href=http://openknot.com>Rob Tuley</a></small></p>
+</footer>
+
+</body>
+</html>
